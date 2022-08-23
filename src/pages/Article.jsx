9a3242642a90d1f0e27ff8483d3d70624fd949b0
BouazziH <104574763+BouazziH@users.noreply.github.com>
2022-08-23 21:40:30 +0200

    lister tout les articles

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,5 +1,35 @@
+import { db } from "../firebase";
+import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
 export default function Article() {
-    return(
-        <h1>article</h1>
-    )
-};
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const articlesRef = query(
+      collection(db, "articles"),
+      orderBy("created", "desc")
+    );
+    onSnapshot(articlesRef, (snapshot) => {
+      setArticles(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+    });
+  }, []);
+
+  return (
+    <>
+      <ul>
+        {articles.map((article) => (
+          <li key={article.id}>
+            <h2>{article.data.titre}</h2>
+            <p>{article.data.articleContenu}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}

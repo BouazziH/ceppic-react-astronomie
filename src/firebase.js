fca7b1ec3fc0ca9c4ae6a83ae80d9a8c0a22ecbe
BouazziH <104574763+BouazziH@users.noreply.github.com>
2022-08-23 21:03:00 +0200

    instalation de firebase et creation de fb.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAJiuCPRxs7Ax0CLIu7OS91TrLV4pLkREc",
+  authDomain: "ceppic-contact-b6a06.firebaseapp.com",
+  projectId: "ceppic-contact-b6a06",
+  storageBucket: "ceppic-contact-b6a06.appspot.com",
+  messagingSenderId: "42577707782",
+  appId: "1:42577707782:web:40b328d3262ad965675487"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
